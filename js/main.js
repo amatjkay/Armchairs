23a1bd25b13b63d2beee3a96fcde8b9f6b95520c
av.korchagin <av.korchagin@tensor.ru>
2020-05-13 12:06:55 +0300

    popup + phoneMask

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector("#close");
+const orderCall = document.querySelector(".header-box__callback");
+const headerText = document.querySelector(".header-box");
+// const phone = document.querySelector("#phone");
 
-cartButton.addEventListener('click', toggleModal);
-modalClose.addEventListener('click', toggleModal);
+orderCall.addEventListener("click", toggleModal);
+modalClose.addEventListener("click", toggleModalClose);
 
 function toggleModal() {
-   modal.classList.toggle("modal--active");
+  modal.classList.toggle("modal_active");
+  headerText.style.display = "none";
+}
+function toggleModalClose() {
+  modal.classList.toggle("modal_active");
+  headerText.style.display = "inline-flex";
 }
 
+// function phoneMask() {
+//   phone.mask("+7 (999) 999-99-99");
+// }
